ce687f90f1ef8c0fda9d2e02ac036bb0b00566b7
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-10 14:26:54 +0100

    7

--- Program.cs
+++ Program.cs
@@ -3,17 +3,17 @@
 	public static void Main()
 	{
 
-		int number1, number2, number3;
+		int number1, number2;
 		Console.Write("\nInput the First Number : ");
 		number1 = int.Parse(Console.ReadLine());
 		Console.Write("\nInput the Second Number : ");
 		number2 = int.Parse(Console.ReadLine());
-		Console.Write("\nInput the Third Number : ");
-		number3 = int.Parse(Console.ReadLine());
 
-		Console.Write("\nFirst Number multiplied by 2: " + number1 * 2);
-		Console.Write("\nSecond Number multiplied by 3: " + number2 * 3);
-		Console.Write("\nThird Number multiplied by 6: " + number3 * 6);
+		Console.Write($"Adding your chosen two numbers together equals: {number1 + number2} \n");
+		Console.Write($"Subtracting your chosen two numbers equals: {number1 - number2} \n");
+		Console.Write($"Multiplying your chosen two numbers equals: {number1 * number2} \n");
+		Console.Write($"Dividing your chosen two numbers equals: {number1 / number2} \n");
+
 		Console.Read();
 
 	}
