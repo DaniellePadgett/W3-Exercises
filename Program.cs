42f7af303414e33628d5a63aed09d14729cb5ac7
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-10 14:17:21 +0100

    6

--- Program.cs
+++ Program.cs
@@ -3,17 +3,17 @@
 	public static void Main()
 	{
 
-		int number1, number2, temp;
+		int number1, number2, number3;
 		Console.Write("\nInput the First Number : ");
 		number1 = int.Parse(Console.ReadLine());
 		Console.Write("\nInput the Second Number : ");
 		number2 = int.Parse(Console.ReadLine());
-		temp = number1;
-		number1 = number2;
-		number2 = temp;
-		Console.Write("\nAfter Swapping : ");
-		Console.Write("\nFirst Number : " + number1);
-		Console.Write("\nSecond Number : " + number2);
+		Console.Write("\nInput the Third Number : ");
+		number3 = int.Parse(Console.ReadLine());
+
+		Console.Write("\nFirst Number multiplied by 2: " + number1 * 2);
+		Console.Write("\nSecond Number multiplied by 3: " + number2 * 3);
+		Console.Write("\nThird Number multiplied by 6: " + number2 * 6);
 		Console.Read();
 
 	}
