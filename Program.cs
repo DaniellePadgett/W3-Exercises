aeb3fe741f19be97d40dcd2008201315f49de607
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-10 13:50:24 +0100

    5

--- Program.cs
+++ Program.cs
@@ -3,10 +3,18 @@
 	public static void Main()
 	{
 
-		Console.WriteLine(-1 + 4 * 6);
-		Console.WriteLine((35 + 5) % 7);
-		Console.WriteLine(14 + -4 * 6 / 11);
-		Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+		int number1, number2, temp;
+		Console.Write("\nInput the First Number : ");
+		number1 = int.Parse(Console.ReadLine());
+		Console.Write("\nInput the Second Number : ");
+		number2 = int.Parse(Console.ReadLine());
+		temp = number1;
+		number1 = number2;
+		number2 = temp;
+		Console.Write("\nAfter Swapping : ");
+		Console.Write("\nFirst Number : " + number1);
+		Console.Write("\nSecond Number : " + number2);
+		Console.Read();
 
 	}
 }
