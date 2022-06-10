d36181cbdf68e7f6fc4d47469543b453eabf2eb4
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-10 13:47:11 +0100

    4

--- Program.cs
+++ Program.cs
@@ -2,12 +2,11 @@
 {
 	public static void Main()
 	{
-		int x = 10;
-		int y = 2;
 
-		int result = x / y;
-
-		Console.WriteLine($"The sum of x and y is {result}");
+		Console.WriteLine(-1 + 4 * 6);
+		Console.WriteLine((35 + 5) % 7);
+		Console.WriteLine(14 + -4 * 6 / 11);
+		Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
 
 	}
 }
