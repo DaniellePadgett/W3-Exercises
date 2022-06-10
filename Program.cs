79fddae498dd04e281607c934768468c29ff2a73
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-10 12:17:02 +0100

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Exercise1
+{
+	public static void Main()
+	{
+		int x = 25;
+		int y = 45;
+
+		int result = x + y;
+
+		Console.WriteLine($"The sum of x and y is {result}");
+
+	}
+}
