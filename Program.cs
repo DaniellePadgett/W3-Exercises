7f8e9d5ecf3fc02628a2540d189193bfc37a41f7
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-10 14:32:58 +0100

    8

--- Program.cs
+++ Program.cs
@@ -4,15 +4,20 @@
 	{
 
 		int number1, number2;
-		Console.Write("\nInput the First Number : ");
+		Console.Write("\nInput your chosen Number : ");
 		number1 = int.Parse(Console.ReadLine());
-		Console.Write("\nInput the Second Number : ");
-		number2 = int.Parse(Console.ReadLine());
 
-		Console.Write($"Adding your chosen two numbers together equals: {number1 + number2} \n");
-		Console.Write($"Subtracting your chosen two numbers equals: {number1 - number2} \n");
-		Console.Write($"Multiplying your chosen two numbers equals: {number1 * number2} \n");
-		Console.Write($"Dividing your chosen two numbers equals: {number1 / number2} \n");
+		Console.Write($"{number1} x 0 equals: {number1 * 0} \n");
+		Console.Write($"{number1} x 1 equals: {number1 * 1} \n");
+		Console.Write($"{number1} x 2 equals: {number1 * 2} \n");
+		Console.Write($"{number1} x 3 equals: {number1 * 3} \n");
+		Console.Write($"{number1} x 4 equals: {number1 * 4} \n");
+		Console.Write($"{number1} x 5 equals: {number1 * 5} \n");
+		Console.Write($"{number1} x 6 equals: {number1 * 6} \n");
+		Console.Write($"{number1} x 7 equals: {number1 * 7} \n");
+		Console.Write($"{number1} x 8 equals: {number1 * 8} \n");
+		Console.Write($"{number1} x 9 equals: {number1 * 9} \n");
+		Console.Write($"{number1} x 10 equals: {number1 * 10} \n");
 
 		Console.Read();
 
