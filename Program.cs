72d1bf5d225e140cfd7c72a52c1f17729f632129
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-10 12:11:04 +0100

    1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Exercise1
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello");
+		Console.WriteLine("Danielle");
+	}
+}
