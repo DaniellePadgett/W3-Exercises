d2eefd745aec97274782221657894d65bb331f95
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-13 14:41:22 +0100

    15

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
 
 
 		Console.WriteLine(RemoveString("Danielle", 1));
-		Console.WriteLine(RemoveString()r("Danielle", 7));
+		Console.WriteLine(RemoveString("Danielle", 7));
 		Console.WriteLine(RemoveString("Danielle", 0));
 	}
 	public static string RemoveString(string name, int n)
