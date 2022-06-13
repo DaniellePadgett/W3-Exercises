c6909ece185081caa1d2ddb3ecef16e1d6104b64
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-13 16:22:58 +0100

    17

--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 {
 	public static void Main()
 	{
-		Console.WriteLine(CharacterSwap("Danielle"));
-		Console.WriteLine(CharacterSwap("Padgett"));
-		Console.WriteLine(CharacterSwap("x"));
-	}
-	public static string CharacterSwap(string namePosition)
-	{
-		return namePosition.Length > 1
-			? namePosition.Substring(namePosition.Length - 1) + namePosition.Substring(1, namePosition.Length - 2) + namePosition.Substring(0, 1) : namePosition;
+		string name;
+		int nameIndex = 0;
+		Console.Write("Input a string : ");
+		name = Console.ReadLine();
+		if (name.Length >= 1)
+		{
+			var s = name.Substring(0, 1);
+			Console.WriteLine(s + name + s);
+		}
 	}
 }
