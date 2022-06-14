83b377b240a66eeded80584f3579fbfbcd730e80
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-14 11:05:01 +0100

    21

--- Program.cs
+++ Program.cs
@@ -2,19 +2,18 @@
 {
 	public static void Main()
 	{
-		Console.WriteLine(result(13, 40));
-		Console.WriteLine(result(50, 21));
-		Console.WriteLine(result(0, 23));
+		int x, y;
+		int result;
+
+		Console.WriteLine("\nInput an integer:");
+		x = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Input another integer:");
+		y = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine(x == 20 || y == 20 || (x + y == 20));
+
 	}
 
-	public static int result(int a, int b)
-	{
-		if (a > b)
-		{
-			return (a - b) * 2;
-		}
-		return b - a;
-	}
 
 
 }
