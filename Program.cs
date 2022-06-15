e4025ce6d7f00d6293a56c34c9faafcdc807cf75
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-15 16:38:06 +0100

    25

--- Program.cs
+++ Program.cs
@@ -2,21 +2,13 @@
 {
 	public static void Main()
 	{
-		string line = "I'm Danielle and my Audi is a bit old lol";
-		string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
-		string word = "";
-		int ctr = 0;
-
-		foreach (String s in words)
+		for (int n = 1; n < (99 + 1); n++)
 		{
-			if (s.Length > ctr)
+			if (n % 2 != 0)
 			{
-				word = s;
-				ctr = s.Length;
+				Console.WriteLine(n.ToString());
 			}
 		}
-
-		Console.WriteLine(word);
 	}
 
 
